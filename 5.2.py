57a9161c98a0349dfb873370baaf5c4eab4cf533
PythonLebo <57559963+PythonLebo@users.noreply.github.com>
2019-11-09 14:45:21 +0200

    Create 5.2.py

---
+++ 5.2.py
@@ -0,0 +1,6 @@
+inp = int(input("Sisestage minutite arv: "))
+vastus = 0
+for num in range(0, inp + 1): 
+    if num % 2 != 0: 
+        vastus = vastus + num
+print("Laikide koguarv on {}.".format(vastus))
