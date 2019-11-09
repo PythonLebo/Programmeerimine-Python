e25992be3b7ad99c816aa39edf53bd5469dd6ddd
PythonLebo <57559963+PythonLebo@users.noreply.github.com>
2019-11-09 18:22:28 +0200

    Create 3.4a.py

---
+++ 3.4a.py
@@ -0,0 +1,9 @@
+inp = int(input("Sisestage minutite arv: "))
+vastus = 0
+idk = 1
+for num in range(0,inp):
+        vastus = vastus + idk
+        idk = idk+2
+print("Laikide koguarv on {}.".format(vastus))
+while 1<1:
+    stupid
