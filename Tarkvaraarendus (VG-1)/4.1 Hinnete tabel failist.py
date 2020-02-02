fb7900a586f5ce83c45e414939afbb17ed641597
RofRuf <60566356+RofRuf@users.noreply.github.com>
2020-02-02 12:13:01 +0200

    Update 4.1 Hinnete tabel failist.py

--- Tarkvaraarendus (VG-1)/4.1 Hinnete tabel failist.py
+++ Tarkvaraarendus (VG-1)/4.1 Hinnete tabel failist.py
@@ -19,3 +19,37 @@ for rida in file:
 file.close()
 
 keskmised(matrix)
+
+#####################################################################
+
+import csv
+hinded = []
+with open("hinded.csv") as fail:
+    csvr = csv.reader(fail, delimiter=',')
+    for rida in csvr:
+        osad = []
+        osad.append(rida[0])
+        del rida[0]
+        for osa in rida:
+            osad.append(int(osa))
+        hinded.append(osad)
+
+def keskmised(i):
+    e = []
+    for a in i:
+        e1 = []
+        w0 = 0
+        for w in a:
+            if type(w) == type('str'):
+                e1.append(w)
+            else:
+                w0 += w
+        ind = len(a) - 1
+        e1.append(round(w0 / ind, 1))
+        e.append(e1)
+    return e
+kesk = keskmised(hinded)
+for u in kesk:
+    aine = u[0]
+    hinne = u[1]
+    print("{}: {}".format(aine, hinne))
