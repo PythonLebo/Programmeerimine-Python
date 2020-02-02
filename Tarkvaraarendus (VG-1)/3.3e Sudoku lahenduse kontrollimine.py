b6d52bb60c0994530a4474e19af3129a0c90c8b0
RofRuf <60566356+RofRuf@users.noreply.github.com>
2020-02-02 12:11:56 +0200

    Update 3.3e Sudoku lahenduse kontrollimine.py

--- Tarkvaraarendus (VG-1)/3.3e Sudoku lahenduse kontrollimine.py
+++ Tarkvaraarendus (VG-1)/3.3e Sudoku lahenduse kontrollimine.py
@@ -47,3 +47,52 @@ if kastid_korras(matrix) and row_check(matrix) and column_check(matrix):
     print("OK")
 else:
     print("VIGA")
+    
+################################################################################################
+
+failn = input("Sisestage faili nimi: ")
+laud = []
+with open(failn, encoding="UTF-8") as fail:
+    for rida in fail:
+        ew = []
+        osad = rida.split()
+        for osa in osad:
+            ew.append(int(osa))
+        laud.append(ew)
+            
+
+def kastid_korras(maatriks):
+    for rea_nurk in range(0, 9, 3):
+        for veeru_nurk in range(0, 9, 3):
+            kast = []
+            for i in range(3):
+                for j in range(3):
+                    kast.append(int(maatriks[rea_nurk + i][veeru_nurk + j]))
+            if sorted(kast) != list(range(1, 10)):
+                return False
+    return True
+
+def read_korras(maatriks):
+    for a in maatriks:
+        if sorted(a) != list(range(1, 10)):
+            return False
+    return True
+
+def veerud_korras(maatriks):
+    a = zip(*maatriks)
+    for i in a:
+        q = []
+        q.append(i)
+        for x in q:
+            if sorted(x) != list(range(1, 10)):
+                return False
+    return True
+
+def sudoku_korras(maatriks):
+    if (kastid_korras(maatriks) == True
+    and read_korras(maatriks) == True
+    and veerud_korras(maatriks) == True):
+        return print("OK")
+    return print("VIGA")
+
+sudoku_korras(laud)
