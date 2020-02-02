9586211e122d175885a0dc0573787e21f767b6cc
RofRuf <60566356+RofRuf@users.noreply.github.com>
2020-02-02 12:04:21 +0200

    Update 1.1 Kalade kaalumine.py

--- Tarkvaraarendus (VG-1)/1.1 Kalade kaalumine.py
+++ Tarkvaraarendus (VG-1)/1.1 Kalade kaalumine.py
@@ -20,3 +20,25 @@ for row in file:
 print(biggest*0.001)
 
 file.close()
+
+###################################################
+
+def kala_kaal(i,o):
+    p = i**3 * o / 100
+    return int(round(p, 0))
+fnimi = input("Sisestage failinimi: ")
+alam = int(input("Sisestage püügi alamõõt: "))
+fti = float(input("Sisestage fultoni tüsedusindeks: "))
+pikkus = []
+kras = 0
+with open(fnimi) as fail:
+    for rida in fail:
+        pikkus.append(int(rida))
+for e in pikkus:
+    if e >= alam:
+        print("Püütud kala kaaluga {} grammi".format(kala_kaal(e, fti)))
+        if kala_kaal(e, fti)> kras:
+            kras = kala_kaal(e, fti)
+    else:
+        print("Kala lasti vette tagasi")
+print("Kõige raskem püütud kala: {} kg".format(float(kras)/1000))
